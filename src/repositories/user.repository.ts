0114b154f1e74c6e260c6b1e78ac34709a560acf
sabasm <smma1992@gmail.com>
2024-11-08 21:59:29 -0700

    WIP: extracting crud into module and also making utils module first for reusage

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,16 +1,4 @@
-import { IUserRepository } from '../interfaces/user.repository.interface';
+import { CrudRepository } from '@smendivil/crud';
 import { UserEntity } from '../user/user.entity';
-
-/**
- * Abstract class for user repositories.
- * Concrete implementations should extend this class.
- */
-export abstract class UserRepository implements IUserRepository {
-    abstract addUser(user: UserEntity): Promise<Partial<UserEntity>>;
-    abstract getUserById(id: string): Promise<Partial<UserEntity> | null>;
-    abstract updateUser(user: UserEntity): Promise<Partial<UserEntity>>;
-    abstract deleteUser(id: string): Promise<boolean>;
-    abstract getAllUsers(): Promise<Partial<UserEntity>[]>;
-}
 
 
